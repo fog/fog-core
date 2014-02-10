259231fb136ca2a8e2284d706f48979e4b73d723
Paul Thornthwaite <tokengeek@gmail.com>
2014-02-10 21:31:45 +0000

    Remove additional parser (XML) code
    Few more "core" classes only dealt with XML parsing so are not really
    core. The deprecated connection was a layer of insulation between XML
    and the Excon connections which can remain in `fog`

--- lib/fog/core.rb
+++ lib/fog/core.rb
@@ -33,9 +33,6 @@ require 'fog/core/wait_for_defaults'
 require 'fog/core/class_from_string'
 require 'fog/core/uuid'
 
-# deprecation wrappers
-require 'fog/core/deprecated/connection'
-
 # service wrappers
 require 'fog/compute'
 require 'fog/identity'
