edd8fac9308db6eb9d9a28474ff0cf6249b1932b
mountkin <moutkin@gmail.com>
2014-02-21 18:29:19 +0800

    Let the wait_for method accept a callback to support variable sleep interval. Use case: http://docs.aws.amazon.com/general/latest/gr/api-retries.html

--- tests/core/wait_for_tests.rb
+++ tests/core/wait_for_tests.rb
@@ -3,6 +3,13 @@ Shindo.tests('Fog#wait_for', 'core') do
     tests('Fog#wait_for').formats(:duration => Integer) do
       Fog.wait_for(1) { true }
     end
+
+    tests('Fog#wait_for with callback').formats(:duration => Integer) do
+      exponential_callback = lambda { |times| [2 ** (times - 1), 3].min }
+      times = 0
+      ret = Fog.wait_for(5, exponential_callback) { (times += 1) > 2 }
+      returns(true) { ret[:duration] < 4 && ret[:duration] >= 3 }
+    end
   end
   
   tests("failure") do
