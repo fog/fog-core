dafbf10eb3fc2aaaed92c8fcb4716703d54a0928
Evan Light <evan@tripledogdare.net>
2014-03-21 18:23:34 -0400

    Ported service_test from shindo

---
+++ spec/service_spec.rb
@@ -0,0 +1,28 @@
+require_relative 'spec_helper'
+
+describe "Fog::Service" do
+  class TestService < Fog::Service
+    recognizes :generic_user, :generic_api_key
+
+    class Real
+      attr_reader :options
+
+      def initialize(opts={})
+        @options = opts
+      end
+    end
+
+    class Mock < Real
+    end
+  end
+
+  it "properly passes headers" do
+    user_agent_hash = {
+      'User-Agent' => 'Generic Fog Client'
+    }
+    params = { :generic_user => "bob", :generic_api_key => '1234', :connection_options => {:headers => user_agent_hash }}
+    service = TestService.new(params)
+
+    assert_equal user_agent_hash, service.options[:connection_options][:headers]
+  end
+end
