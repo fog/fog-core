d05e6bbbcc072d7a14516f25ca29c448f606d171
mountkin <moutkin@gmail.com>
2014-04-02 20:24:33 +0800

    change the default interval to a proc that returns exponential interval

--- lib/fog/core/wait_for.rb
+++ lib/fog/core/wait_for.rb
@@ -2,8 +2,9 @@ module Fog
   def self.wait_for(timeout=Fog.timeout, interval=Fog.interval, &block)
     duration = 0
     start = Time.now
+    retries = 0
     until yield || duration > timeout
-      sleep(interval.to_f)
+      sleep(interval.respond_to?(:call) ? interval.call(retries += 1).to_f : interval.to_f)
       duration = Time.now - start
     end
     if duration > timeout
--- lib/fog/core/wait_for_defaults.rb
+++ lib/fog/core/wait_for_defaults.rb
@@ -1,11 +1,15 @@
 module Fog
-  @interval = 1
+  @interval = lambda { |retries| [2 ** (retries - 1), @max_interval].min }
   def self.interval
     @interval
   end
 
   def self.interval=(interval)
-    raise ArgumentError, "interval must be non-negative" unless interval >= 0
+    if interval.kind_of?(Proc)
+      raise ArgumentError, "interval proc must return a possitive" unless interval.call(1) >= 0
+    else
+      raise ArgumentError, "interval must be non-negative" unless interval >= 0
+    end
     @interval = interval
   end
   
@@ -17,5 +21,15 @@ module Fog
   def self.timeout=(timeout)
     raise ArgumentError, "timeout must be non-negative" unless timeout >= 0
     @timeout = timeout
+  end
+  
+  @max_interval = 60
+  def self.max_interval
+    @max_interval
+  end
+
+  def self.max_interval=(interval)
+    raise ArgumentError, "interval must be non-negative" unless interval >= 0
+    @max_interval = interval
   end
 end
