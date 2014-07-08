a436a5dc3b7347993fcba9312ecfec134af17ad4
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-07-08 10:25:49 -0300

    Added code to fix the issue when converting XMPRCP::DateTime objects

--- lib/fog/core/attributes.rb
+++ lib/fog/core/attributes.rb
@@ -55,6 +55,8 @@ module Fog
             def #{name}=(new_#{name})
               attributes[:#{name}] = if new_#{name}.nil? || new_#{name} == "" || new_#{name}.is_a?(Time)
                 new_#{name}
+              elsif new_#{name}.respond_to?(:to_time)
+                new_#{name}.to_time
               else
                 Time.parse(new_#{name})
               end
