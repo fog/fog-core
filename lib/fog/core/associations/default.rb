3fe06c393a01dfd8edc2013cf3c222d5f058865c
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-08-27 13:35:39 -0300

    Added feature to overwrite keys on hash of attributes generation
    Fog already has support to inform that an attribute might be
    initialized with another name. This feature makes possible to map
    attributes that aren't ruby friend to friendly ones.
    
    This code makes possible to map the attributes that are ruby friendly
    to the methods that the provider need/wants.
    
    Together, the `aliases` and `as` options will map the inialization
    of the attributes and the generation of proper attribute names.

--- lib/fog/core/associations/default.rb
+++ lib/fog/core/associations/default.rb
@@ -1,22 +1,28 @@
 module Fog
   module Associations
     class Default
-      attr_reader :model, :name, :aliases
+      attr_reader :model, :name, :aliases, :as
 
       def initialize(model, name, collection_name, options)
         @model = model
         @name = name
         model.associations[name] = collection_name
         @aliases = options.fetch(:aliases, [])
+        @as = options.fetch(:as, name)
         create_setter
         create_getter
         create_aliases
+        create_mask
       end
 
       def create_aliases
         Array(aliases).each do |alias_name|
           model.aliases[alias_name] = name
         end
+      end
+
+      def create_mask
+        model.masks[name] = as
       end
     end
   end
