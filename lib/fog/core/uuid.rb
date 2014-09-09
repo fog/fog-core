24a0a24bd89c0acdaa0d2c85ce44d04311b06cad
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-09-09 14:05:28 -0300

    Fix ruby 1.8.7 build
    Rubocop asked to use `format` instead of `%` to format string.
    But we still support ruby 1.8.7 and `format` on it is private.

--- lib/fog/core/uuid.rb
+++ lib/fog/core/uuid.rb
@@ -10,7 +10,7 @@ module Fog
           ary = SecureRandom.random_bytes(16).unpack("NnnnnN")
           ary[2] = (ary[2] & 0x0fff) | 0x4000
           ary[3] = (ary[3] & 0x3fff) | 0x8000
-          "%08x-%04x-%04x-%04x-%04x%08x".format(ary)
+          "%08x-%04x-%04x-%04x-%04x%08x" % ary
         end
       end
 
