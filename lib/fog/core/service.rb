12558de052858b4a39527251a3d71ee50deb49d5
Frederick Cheung <frederick.cheung@gmail.com>
2015-03-05 14:02:30 +0000

    value is an arbitrary object and may not implement #== with string argument
    On ruby 2.2 the changed line can emit a warning along the lines of
    
    Comparable#== will no more rescue exceptions of #<=> in the next release.
    
    One case where this happens is when value is an instance of Time

--- lib/fog/core/service.rb
+++ lib/fog/core/service.rb
@@ -163,7 +163,7 @@ module Fog
           value_string = value.to_s.downcase
           if value.nil?
             options.delete(key)
-          elsif value == value_string.to_i.to_s
+          elsif value_string.to_i.to_s == value
             options[key] = value.to_i
           else
             options[key] = case value_string
