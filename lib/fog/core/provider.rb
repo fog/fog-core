a59a5be0647ae900d5f25033e1cbd9db4f3f3586
Yauhen Artsiukhou <jsirex@gmail.com>
2023-06-23 01:21:02 +0200

    rubocop: Fix Layout/DotPosition

--- lib/fog/core/provider.rb
+++ lib/fog/core/provider.rb
@@ -18,11 +18,11 @@ module Fog
       private
 
       def underscore_name(string)
-        string.gsub(/::/, "/").
-          gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-          gsub(/([a-z\d])([A-Z])/,'\1_\2').
-          tr("-", "_").
-          downcase
+        string.gsub(/::/, "/")
+              .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
+              .gsub(/([a-z\d])([A-Z])/,'\1_\2')
+              .tr("-", "_")
+              .downcase
       end
     end
 
