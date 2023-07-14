c8af174cc47ef7b2d23054e161ecea81a5a5bff0
Yauhen Artsiukhou <jsirex@gmail.com>
2023-07-14 14:41:18 +0200

    Use SecureRandom.uuid since ruby 2.x

--- lib/fog/core/uuid.rb
+++ lib/fog/core/uuid.rb
@@ -4,16 +4,10 @@ module Fog
   class UUID
     class << self
       def uuid
-        if supported?
-          SecureRandom.uuid
-        else
-          ary = SecureRandom.random_bytes(16).unpack("NnnnnN")
-          ary[2] = (ary[2] & 0x0fff) | 0x4000
-          ary[3] = (ary[3] & 0x3fff) | 0x8000
-          "%08x-%04x-%04x-%04x-%04x%08x" % ary
-        end
+        SecureRandom.uuid
       end
 
+      # :nodoc: This method is used by other plugins, so preserve it for the compatibility
       def supported?
         SecureRandom.respond_to?(:uuid)
       end
