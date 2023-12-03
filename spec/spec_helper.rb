773a97b4bd67d564559ee9c9cd4c09d6ebd7deb1
micwoj92 <45581170+micwoj92@users.noreply.github.com>
2023-12-03 19:24:40 +0100

    last bits

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,3 @@
-if ENV["COVERAGE"]
-  require "coveralls"
-  require "simplecov"
-
-  SimpleCov.start do
-    add_filter "/spec/"
-  end
-end
-
 # Set home outside of real user
 require "tmpdir"
 ENV["HOME"] = Dir.mktmpdir("foghome")
